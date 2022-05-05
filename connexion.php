5a488892ff7f6ae7ec14b512e87c07538a9146e2
Questiero <19290903+Questiero@users.noreply.github.com>
2022-05-05 08:44:33 +0200

    oui

--- connexion.php
+++ connexion.php
@@ -1,3 +1,13 @@
 <?php session_start(); ?>
 
-oui
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TierLection</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="Tierlection.css"/>
+</head>
+<body>
+
+</body>
+</html>
--- electionList.php
+++ electionList.php
@@ -1,1 +1,13 @@
 <?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TierLection</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="Tierlection.css"/>
+</head>
+<body>
+
+</body>
+</html>
