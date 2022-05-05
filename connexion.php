2b0863adec64338cfbbdd0e27a080ace357124ee
Questiero <19290903+Questiero@users.noreply.github.com>
2022-05-05 10:50:54 +0200

    page connexion

--- connexion.php
+++ connexion.php
@@ -9,5 +9,89 @@
 </head>
 <body>
 
+    <div class="panel">
+        <form action="connexion.php" method="post">
+            <div id="valeurs">
+                <div>
+                    <label for="username">Nom d'utilisateur</label>
+                    <input type="text" id="username" name="username" size="20"/>
+                </div>
+                <div>
+                    <label for="password">Mot de passe</label>
+                    <input type="password" id="password" name="password"/>
+                </div>
+
+                <div>
+                    <label id="mess_erreur"></label>
+                </div>
+                    
+            </div>
+
+            <div id="connection">
+                <input type="submit" value="Connection"/>
+            </div>          
+        </form>
+    </div>
+
+    <div id="errors">
+
+        <?php
+
+                $connexionData = getConnexionData($_POST["username"]);
+
+                if(is_null($connexionData)) {
+                    echo "Nom d'utilisateur inconnu";
+                } else {
+
+                    $dbPassword = $connexionData[1];
+                    $password = hash('sha384', $_POST['password'] . $connexionData[0]);
+
+                    if($password == $dbPassword) {
+
+                        $_SESSION["name"] = $_POST["username"];
+
+                        header("Location: <ital>index.php</ital>");
+
+                    } else {
+                        echo "Mot de passe inconnu";
+                    }
+
+                }
+
+            ?>
+
+    </div>
+
 </body>
 </html>
+
+<?php
+
+    function getConnexionData($username) {
+
+        $datas = null;
+
+        $connection = new PDO(
+            "mysql:host=mysql-questiero.alwaysdata.net;dbname=questiero_tierlection",
+            "questiero_tl",
+            "tierlection"
+        );
+
+        $query = "SELECT salt, password FROM user WHERE username = :username";
+        $statement = $connection->prepare($query);
+
+        // Bind value and execute query
+        $statement->bindValue(":username", $username, PDO::PARAM_STR);
+        $statement->execute();
+
+        // Browse the results
+        foreach ($statement as $row) {
+            $datas[0] = $row['salt'];
+            $datas[1] = $row['password'];
+        }
+
+        return $datas;
+
+    }
+
+?>
