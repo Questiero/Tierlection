57053cdea0ee138f4306cd7dd3898f3a21041bab
Touilde <mathilde.val88@gmail.com>
2022-05-14 17:01:24 +0200

    Credits

--- Credits.php
+++ Credits.php
@@ -13,11 +13,24 @@
 		<?php
 		echo 	"<div id=\"titre\">
                 	<img id=\"tierlection-logo\" src=\"data/TierLection-Logo.png\">
+                    <h1>Crédits</h1>
             	</div>
-            	<div id=\"Introduction\">
+            	<div id=\"introduction\">
             		<p>Nous sommes quatre étudiants en Licence 2 Mathématiques Informatique Appliquées aux Sciences Humaines et Sociales parcours Sciecnes Cognitives.<br>Notre but avec le site TierLection est de pouvoir créer ou participer à des élections par comparaisons. </p>
+            	</div>
+            	<div id=\"presentation\">
+            			<h2>Alexandre</h2>
+                        <p>Texte</p>
+                        <h2>Jules</h2>
+                        <p>Texte</p>
+                        <h2>Chloé</h2>
+                        <p>Texte</p>
+                        <h2>Mathilde</h2>
+                        <p>Texte</p>
             	</div>
         ";
 		?>
+
+        <script type="text/javascript" src="logoIndex.js"></script>
 	</body>
 </html>
