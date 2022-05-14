f9fe83ea9b90fcd440d95165b12eb39567c0785d
Touilde <mathilde.val88@gmail.com>
2022-05-14 14:55:09 +0200

    ReCredit

---
+++ Credits.php
@@ -0,0 +1,23 @@
+<?php 
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Crédits</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="TierLection.css"/>
+</head>
+	<body>
+		<?php
+		echo 	"<div id=\"titre\">
+                	<img id=\"tierlection-logo\" src=\"data/TierLection-Logo.png\">
+            	</div>
+            	<div id=\"Introduction\">
+            		<p>Nous sommes quatre étudiants en Licence 2 Mathématiques Informatique Appliquées aux Sciences Humaines et Sociales parcours Sciecnes Cognitives.<br>Notre but avec le site TierLection est de pouvoir créer ou participer à des élections par comparaisons. </p>
+            	</div>
+        ";
+		?>
+	</body>
+</html>
